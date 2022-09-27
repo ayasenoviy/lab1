1f788bfbf70384793479fe23ccbffd6b7c224766
ayasenoviy <ayasenoviy@gmail.com>
2022-09-27 17:38:01 +0300

    add file fibonachi.py

---
+++ fibonachi.py
@@ -0,0 +1,23 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+     
+    if n < 0:
+        print("Incorrect input")
+         
+    elif n == 0:
+        return 0
+       
+    elif n == 1:
+        return b
+
+    else:
+        for i in range(1, n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+n = input("input number: ")
+for j in range(1,int(n)):
+    print(fibonacci(j))
